956564ec13f7df831513311cad4dae159e53b0ea
qujie <qujie@meizu.com>
2025-06-20 10:42:27 +0800

    feat: enhance redirection logic for non-editor users to fetch installed apps

--- web/utils/app-redirection.ts
+++ web/utils/app-redirection.ts
@@ -1,12 +1,25 @@
+import { fetchInstalledAppList } from '@/service/explore'
 import type { AppMode } from '@/types/app'
+import { basePath } from './var'
+import Toast from '@/app/components/base/toast'
 
-export const getRedirection = (
+export const getRedirection = async (
   isCurrentWorkspaceEditor: boolean,
   app: { id: string, mode: AppMode },
   redirectionFunc: (href: string) => void,
 ) => {
   if (!isCurrentWorkspaceEditor) {
-    redirectionFunc(`/app/${app.id}/overview`)
+    // redirectionFunc(`/app/${app.id}/overview`)
+    try {
+      const { installed_apps }: any = await fetchInstalledAppList(app.id) || {}
+      if (installed_apps?.length > 0)
+        window.open(`${basePath}/explore/installed/${installed_apps[0].id}`, '_blank')
+      else
+        throw new Error('No app found in Explore')
+    }
+    catch (e: any) {
+      Toast.notify({ type: 'error', message: `${e.message || e}` })
+    }
   }
   else {
     if (app.mode === 'workflow' || app.mode === 'advanced-chat')
